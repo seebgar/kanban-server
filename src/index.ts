0fd751c2fc5b931deda68e24c41c7fdc304e211f
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2022-05-17 14:06:25 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import 'module-alias/register';
+import 'dotenv/config';
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+
+import createDatabaseConnection from 'database/createConnection';
+import { addRespondToResponse } from 'middleware/response';
+import { authenticateUser } from 'middleware/authentication';
+import { handleError } from 'middleware/errors';
+import { RouteNotFoundError } from 'errors';
+
+import { attachPublicRoutes, attachPrivateRoutes } from './routes';
+
+const establishDatabaseConnection = async (): Promise<void> => {
+  console.log('[TASK] --> Database connection START');
+  try {
+    console.log({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      //entities: Object.values(entities),
+      synchronize: true,
+    });
+    await createDatabaseConnection();
+  } catch (error) {
+    console.log('[ERROR] --> Database connection ERROR');
+    console.log(error);
+  }
+};
+
+const initializeExpress = (): void => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  app.use(addRespondToResponse);
+
+  attachPublicRoutes(app);
+
+  app.use('/', authenticateUser);
+
+  attachPrivateRoutes(app);
+
+  app.use((req, _res, next) => next(new RouteNotFoundError(req.originalUrl)));
+  app.use(handleError);
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('[UP] --> Listening on ' + 3000);
+  });
+};
+
+const initializeApp = async (): Promise<void> => {
+  await establishDatabaseConnection();
+  initializeExpress();
+};
+
+initializeApp();
